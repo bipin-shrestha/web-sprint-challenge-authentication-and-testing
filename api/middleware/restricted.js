d7a342fe100bee8370c498c0e4fc8909bdf8e3d1
bipin-shrestha <iambpn.shrestha@gmail.com>
2021-04-18 13:09:18 -0700

    Completed restricted

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -14,24 +14,25 @@
 
 const restricted = (req, res, next) => {
 try {
-const token = req.headers.authorization?.split('')[1];
-if(token){
-  jwt.verify(token, JWT_SECRET, (error, decodedToken) => {
-    if(error){
-      res.status(401).json({ message: 'Token Invalid'})
-    } else {
-      req.decodedToken = decodedToken;
-      next();
-    }
-  })
-} else {
-  res.status(401).json({ message: 'Token Required' });
-}
-}
-catch(error){
-  console.log(error);
-  res.status(500).json({message: 'Error validating credentials'});
-}
+  const token = req.headers.authorization?.split(' ')[1];
+  console.log(token);
+  if(token){
+    jwt.verify(token, JWT_SECRET, (error, decodedToken) => {
+      if(error){
+        res.status(401).json({ message: 'Token Invalid'})
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ message: 'Token Required' });
+  }
+  }
+  catch(error){
+    console.log(error);
+    res.status(500).json({message: 'Error validating credentials'});
+  }
 }
 
 
